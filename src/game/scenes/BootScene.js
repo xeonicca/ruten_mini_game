1e6c159d03774af6bdc1285f070db648536d5f71
Steve Yu <soundedrain@gmail.com>
2018-09-15 11:18:17 +0800

    init commit

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -3,7 +3,8 @@ import sky from '@/game/assets/sky.png'
 import bomb from '@/game/assets/bomb.png'
 import thudMp3 from '@/game/assets/thud.mp3'
 import thudOgg from '@/game/assets/thud.ogg'
-
+import platform from '@/game/assets/platform.png'
+import dude from '@/game/assets/dude.png'
 
 export default class BootScene extends Scene {
   constructor () {
@@ -14,6 +15,8 @@ export default class BootScene extends Scene {
     this.load.image('sky', sky)
     this.load.image('bomb', bomb)
     this.load.audio('thud', [thudMp3, thudOgg])
+    this.load.image('ground', platform);
+    this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
   }
 
   create () {
--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -1,26 +1,121 @@
 import { Scene } from 'phaser'
 
-
 export default class PlayScene extends Scene {
-  constructor () {
+  constructor() {
     super({ key: 'PlayScene' })
+    this.maxBombs = 10
+    this.bombs = []
   }
 
-  create () {
-    this.add.image(400, 300, 'sky')
-
+  addBomb() {
     const bomb = this.physics.add.image(400, 200, 'bomb')
     bomb.setCollideWorldBounds(true)
     bomb.body.onWorldBounds = true // enable worldbounds collision event
-    bomb.setBounce(1)
-    bomb.setVelocity(200, 20)
+    bomb.setBounce(.4)
+    bomb.setVelocity(100, 20)
+    bomb.setCollideWorldBounds(true);
+
+
+    return bomb
+  }
+
+  addPlayer() {
+    const player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    player.body.setGravityY(300)
+
+
+    return player
+  }
+
+  create() {
+    this.add.image(400, 300, 'sky')
+    const platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    this.player = this.addPlayer()
+
+    let timer = setInterval(() => {
+      if (this.maxBombs > this.bombs.length) {
+        const bomb = this.addBomb()
+        this.physics.add.collider(bomb, platforms);
+        this.physics.add.overlap(this.player, bomb, this.collectStar.bind(this), null, this);
+        this.bombs.push(bomb)
+      } else {
+        // clearInterval(timer)
+      }
+    }, 500)
+
+
+
+    this.physics.add.collider(this.player, platforms);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
 
     this.sound.add('thud')
     this.physics.world.on('worldbounds', () => {
-      this.sound.play('thud', { volume: 0.75 })
+      // this.sound.play('thud', { volume: 0.75 })
     })
   }
 
-  update () {
+  collectStar(player, bomb) {
+    this.bombs.splice(this.bombs.indexOf(bomb), 1)
+    bomb.disableBody(true, true);
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play('left', true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play('right', true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play('turn');
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 }
